d2bdecb3e5a6d647ab4c864d8dfa45c6890d58a3
rubabanat <rubabanat1996@gmail.com>
2021-03-30 20:44:39 +0300

    validitor

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,11 +1,10 @@
-module.exports = (req,res,next) =>{
-    if (!req.params.id){
+'use strict';
+
+module.exports=(req,res,next)=>{
+    if(req.params.id){
         next();
-        
-    } else {
-        res.status(500);
-        res.send({ error : 'Error'});
-        next('INVALID ID');
+    }else{
+        next('Not Found');
     }
 }
 
